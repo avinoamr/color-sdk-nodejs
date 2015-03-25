c802228e69f851f59d447c8bb718d0e03308b089
Roi Avinoam <avinoamr@gmail.com>
2015-03-25 12:16:32 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,4 @@ var sdk = new color.SDK( process.argv[ 2 ] );
 sdk.write( "install", { user: 15, device: "iPhone" } );
 sdk.write( "install", { user: 16, device: "Android" } );
 sdk.write( "install", { user: 17, device: "iPad" } );
+sdk.end();
